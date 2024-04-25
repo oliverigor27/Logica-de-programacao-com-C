9f6580e376ffaf9afec4898d5870cc1dcfd2da4f
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-25 18:21:23 -0300

    feat: create new lessons

---
+++ src/Exec01.c
@@ -0,0 +1,12 @@
+// Num and Format Specifiers lesson
+
+#include <stdio.h>
+
+int main()
+{
+    int myNum = 10;
+
+    printf("The number is: %d\n", myNum);
+
+    return 0;
+}
---
+++ src/Exec02.c
@@ -0,0 +1,12 @@
+// Strings lesson
+
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Hello World!";
+
+    printf("%s\n", name);
+
+    return 0;
+}
