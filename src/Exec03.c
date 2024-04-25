e1f3b54b5feb230b70b2b328a5aa42abd07b72d6
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-25 19:41:22 -0300

    feat: create a new lesson

---
+++ src/Exec03.c
@@ -0,0 +1,21 @@
+// if and else exec
+
+#include <stdio.h>
+
+int main()
+{
+    int varOne = 10;
+    int varTwo = 15;
+
+    int result = varOne + varTwo;
+
+    if(result != 10)
+    {
+        printf("Wrong Result!\n");
+    } else 
+    {
+        printf("Right result!\n");
+    }
+
+    return 0;
+}
